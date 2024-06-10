fd9015728ea51c0e0d7e7669f205b23fe2677d7d
drnomader <drnomader@gmail.com>
2024-06-11 08:26:19 +0900

    add java files

---
+++ src/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava { // 클래스
+	
+	public static void main(String[] args) { // main() 메서드 : 프로그램의 시작점
+		System.out.println("hello java"); // 값을 콘솔에 출력
+	} // main() 메서드 끝, {} 으로 구분
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,9 @@
+public class HelloJava2 {
+	
+	public static void main(String[] args) {
+		// 줄단위 순차 실행
+		System.out.println("hello java1");
+		System.out.println("hello java2");
+		System.out.println("hello java3");
+	}
+}
